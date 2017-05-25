8906267b3e3c396b652cd1735bcc9bd81c52a28c
Ted Chen <ubervideodisc@yahoo.com>
2017-05-25 14:03:59 -0700

    Add description scraping. Write to output file
    actually writing to the output file is better than just printing, you know?

--- botw_scrapeJsonToYml.py
+++ botw_scrapeJsonToYml.py
@@ -47,7 +47,7 @@ def main(argv=None):
     # Otherwise, it's a search query
     else:
         foundPosts = []
-        reddit = praw.Reddit(client_id='<client>',
+        reddit = praw.Reddit(client_id='<id>',
                              client_secret='<secret>',
                              user_agent='windows:py-sb-botw:0.1 (by /u/6_lasers)')
         
@@ -70,8 +70,8 @@ def main(argv=None):
     # TODO: this might change (as well as other text patterns this script relies on)
     for post in foundPosts:
         if "This weeks build topic" in post['data']['selftext']:
-            print "  - title: " + post['data']['title'].split(":")[1].strip()
-            print "    url: " + post['data']['url']
+            outputYmlFile.write("  - title: " + post['data']['title'].split(":")[1].strip() + "\n")
+            outputYmlFile.write("    url: " + post['data']['url'] + "\n")
             
             # Compile list of entries
             entries = []
@@ -80,32 +80,47 @@ def main(argv=None):
                 # Convention is that a "Current entrees" (sic) line begins the entries
                 if "Current entrees" in line:
                     entries_started = True
-                # and a bunch of dashes ends the entries
-                elif "-------------" in line:
-                    entries_started = False
-                # Get all non-blank lines in between
-                elif entries_started and line != "":
-                    # Format looks something like:
-                    # [The last remnant by TrIpTiCuS] (https://www.reddit.com/r/starbound/comments/6bhc4g/botwthe_last_remnant_repost_because_i_forgot_botw/)
-                    expmatch = re.search("\[(.*)\s+by\s+(.*)\]\s*\((.*)\)", line)
-                    if expmatch:
-                        entries.append(expmatch.groups())
+                elif entries_started:
+                    # and a bunch of dashes ends the entries
+                    if "-------------" in line:
+                        entries_started = False
+                    # Get all non-blank lines in between
+                    elif line != "":
+                        # Format looks something like:
+                        # [The last remnant by TrIpTiCuS] (https://www.reddit.com/r/starbound/comments/6bhc4g/botwthe_last_remnant_repost_because_i_forgot_botw/)
+                        expmatch = re.search("\[(.*)\s+by\s+(.*)\]\s*\((.*)\)", line)
+                        if expmatch:
+                            entries.append(expmatch.groups())
+                        else:
+                            # Due to typos, the expression sometimes fails, so fail gracefully
+                            # TODO: is there any way programmatically to salvage the line?
+                            print "ERROR on line: " + line
+                # Convention is that the description begins with "Build Criteria"
+                elif "Build Criteria" in line:
+                    desc_started = True
+                elif desc_started:
+                    # and description ends with "Winners will be chosen" or a bunch of dashes
+                    if "-------------" in line or "Winners will be chosen" in line:
+                        desc_started = False
+                    # All the lines in between make up the description
                     else:
-                        # Due to typos, the expression sometimes fails, so fail gracefully
-                        # TODO: is there any way programmatically to salvage the line?
-                        print "ERROR on line: " + line
+                        desc += "      " + line + "\n"
             
+            # Print description
+            outputYmlFile.write("    desc: |\n")
+            outputYmlFile.write(desc)
+
             # Now that we have the list, print it in YAML format.
             # I don't use a YAML dumper because it doesn't
             # get the format the way I like it..
-            print "    entries:"
+            outputYmlFile.write("    entries:\n")
             for entry in entries:
-                print "    - entrant: \"{0}\"".format(entry[1])
-                print "      title: \"{0}\"".format(entry[0])
-                print "      url: " + entry[2]
+                outputYmlFile.write("    - entrant: \"{0}\"\n".format(entry[1]))
+                outputYmlFile.write("      title: \"{0}\"\n".format(entry[0]))
+                outputYmlFile.write("      url: " + entry[2] + "\n")
             # Have to fill in winner, runnerup by hand since that comes from next week's post
-            print "    winner: [\"\"]"
-            print "    runnerup: \"\""
+            outputYmlFile.write("    winner: [\"\"]\n")
+            outputYmlFile.write("    runnerup: \"\"\n")
     
     outputYmlFile.close()
     
